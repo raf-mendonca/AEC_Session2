7ceb687317307d26f7f813a89973ef4c718255e4
raf-mendonca <rapedrao@hotmail.com>
2020-04-18 11:35:27 -0400

    Cours1 Formulaires Accordeon

---
+++ Integration2/Cours1/Formulaires/js/script.js
@@ -0,0 +1,12 @@
+/*Formulaire du chat*/
+
+function validerFormulaire() {
+    var x= document.forms["monFormulaire"].value;
+    if (x == ""){
+        alert("Veuillez remplir le formulaire");
+        return false;
+    }
+    else {
+
+    }
+}
