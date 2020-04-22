d04e8ea0163a211c718c68fe99f4073df82066c0
raf-mendonca <rapedrao@hotmail.com>
2020-04-21 20:52:40 -0400

    Envoie thelastdanse React

---
+++ Programmation2/thelastdanse/build/precache-manifest.c5a38158cbe2fd527a643ebcfacb1d6c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "28c57ecb2368de37f717b26f6f416bc1",
+    "url": "./index.html"
+  },
+  {
+    "revision": "0eac6d7e43afbd527e1c",
+    "url": "./static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "701e9345eaec3ea8f059",
+    "url": "./static/js/2.c673f5e9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.c673f5e9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0eac6d7e43afbd527e1c",
+    "url": "./static/js/main.048bc365.chunk.js"
+  },
+  {
+    "revision": "e109fad1f05857380927",
+    "url": "./static/js/runtime-main.68438d4b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "./static/media/logo.5d5d9eef.svg"
+  }
+]);
