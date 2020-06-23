540ca5f103215e3431bc280a903adc3b321a69af
raf-mendonca <rapedrao@hotmail.com>
2020-06-23 19:19:19 -0400

    premier envoie rebrandly

---
+++ Programmation2/rebrandly/build/precache-manifest.b2ca022d7e727e335ab6a567022143b5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b73b3320e0849fe98c0490a971e7b61f",
+    "url": "./index.html"
+  },
+  {
+    "revision": "07c4a1a2275b81865b5f",
+    "url": "./static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "fb0fd48ab9d2ba888e4e",
+    "url": "./static/js/2.4354647a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.4354647a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "07c4a1a2275b81865b5f",
+    "url": "./static/js/main.fb94ad97.chunk.js"
+  },
+  {
+    "revision": "bf121f99ce1dafbc798b",
+    "url": "./static/js/runtime-main.e1da497a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "./static/media/logo.5d5d9eef.svg"
+  }
+]);
