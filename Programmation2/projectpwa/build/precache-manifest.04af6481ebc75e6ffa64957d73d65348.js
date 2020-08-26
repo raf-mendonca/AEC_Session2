0d8b898b9712bdf415bd8f911c13cf338d1dadeb
raf-mendonca <rapedrao@hotmail.com>
2020-08-25 21:53:05 -0400

    pwa test shany2

---
+++ Programmation2/projectpwa/build/precache-manifest.04af6481ebc75e6ffa64957d73d65348.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "02257b3492e40a6e25649aa252209268",
+    "url": "./index.html"
+  },
+  {
+    "revision": "a64bd3b5141a1c7d81f7",
+    "url": "./static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "ba1e64d718f485549bc3",
+    "url": "./static/js/2.5d78a6f2.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "./static/js/2.5d78a6f2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a64bd3b5141a1c7d81f7",
+    "url": "./static/js/main.6d31a4e3.chunk.js"
+  },
+  {
+    "revision": "96608032757e511fd64e",
+    "url": "./static/js/runtime-main.277795df.js"
+  },
+  {
+    "revision": "3abe438f0c273281baa9da6fa9fcc7d2",
+    "url": "./static/media/pwa.3abe438f.png"
+  }
+]);
